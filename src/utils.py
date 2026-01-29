9bf1ed83368149f5f192aef6eb7253522e534887
walekr12 <161548283+walekr12@users.noreply.github.com>
2026-01-30 03:26:35 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -13,7 +13,7 @@ security = HTTPBearer()
 
 GEMINICLI_USER_AGENT = "GeminiCLI/0.1.5 (Windows; AMD64)"
 
-ANTIGRAVITY_USER_AGENT = "antigravity/1.11.3 windows/amd64"
+ANTIGRAVITY_USER_AGENT = "antigravity/1.15.8 windows/amd64"
 
 # OAuth Configuration - 标准模式
 CLIENT_ID = "681255809395-oo8ft2oprdrnp9e3aqf6av3hmdib135j.apps.googleusercontent.com"
